59de5189d24e3d064c74854e3a680124608a832e
Niranjankumar N <niranjankumar.n@quadrasystems.net>
2025-07-31 12:16:39 +0530

    add copilotskill master

---
+++ bot.js
@@ -0,0 +1,19 @@
+const { ActivityHandler } = require('botbuilder');
+
+class EmptyBot extends ActivityHandler {
+    constructor() {
+        super();
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity('Hello world!');
+                }
+            }
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+}
+
+module.exports.EmptyBot = EmptyBot;
